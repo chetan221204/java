f3be5cab0724a5b438eeccc6baf2940b4a8ba7d9
chetan221204 <chetanchauhan247@gmail.com>
2025-04-16 14:16:10 +0530

    max of arrray

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,8 +1,2 @@
-public class sum {
-    public static void main(String[] args) {
-        int a=123;
-        int original=a;
-        while (a>0) {
-            int i =a%10;
-            int sum=0;
-            sum=i+i;
+ // int MAX=arr[n];
+            // System.out.println(MAX);
