f438a45facd093f6afaa4083f41175fda9b07d68
chetan221204 <chetanchauhan247@gmail.com>
2025-04-16 12:56:17 +0530

    sum of digit

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args) {
+        int a=123;
+        int original=a;
+        while (a>0) {
+            int i =a%10;
+            int sum=0;
+            sum=i+i;
